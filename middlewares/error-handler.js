f6a4878caae8183ae2d2f952edb901853c96cac7
iceyisaak <me@iceyisaak.com>
2022-05-26 12:26:49 +0200

    Updated errorHandlerMiddleware

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,12 +1,28 @@
-const { CustomAPIError } = require('../errors');
+// const { CustomAPIError } = require('../errors');
 const { StatusCodes } = require('http-status-codes');
 
 const errorHandlerMiddleware = (err, req, res, next) => {
 
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
+  // console.log('err:', err);
+  // console.log('err.code:', err.code);
+  // console.log('err.statusCode:', err.statusCode);
+
+  let customError = {
+    // set default
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something went wrong."
+  };
+
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message });
+  // }
+
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field. Please enter new value.`;
+    customError.statusCode = StatusCodes.BAD_REQUEST;
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ err });
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ err });
+  return res.status(customError.statusCode).send({ msg: customError.msg });
 };
 
 module.exports = errorHandlerMiddleware;
