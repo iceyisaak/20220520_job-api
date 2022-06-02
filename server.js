a41ece3d856af05e7899da134c65c063cd5b6dea
iceyisaak <me@iceyisaak.com>
2022-06-02 14:28:34 +0200

    Added SwaggerUI

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const express = require('express');
 const app = express();
 
@@ -35,8 +40,13 @@ app.use(cors());
 app.use(xss());
 
 app.get('/', (req, res) => {
-  res.send('job api');
+  res.send(`
+    <h1>Jobs API</h1>
+    <a href='/api-docs'>See Documentation</a>
+  `);
 });
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // app.use('/api/v1/', mainRouter);
 app.use('/api/v1/auth', authRouter);
